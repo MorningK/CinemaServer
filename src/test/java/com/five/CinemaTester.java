f4bc8c49ac3007a1caeb07f48583c4872edb953f
skylhd <dickylhd@gmail.com>
2017-06-08 17:52:07 +0900

    Finish cinema tester

--- src/test/java/com/five/CinemaTester.java
+++ src/test/java/com/five/CinemaTester.java
@@ -1,5 +1,6 @@
 package com.five;
 
+import com.five.cinema.dao.CinemaDao;
 import com.five.cinema.model.Cinema;
 import com.five.cinema.repository.CinemaRepository;
 import com.five.cinema.service.CinemaService;
@@ -35,13 +36,16 @@ public class CinemaTester {
 
     @Autowired
     private CinemaService cinemaService;
+    @Autowired
+    private CinemaDao cinemaDao;
 
     private ArrayList<Cinema> cinemas = new ArrayList<Cinema>();
+    private ArrayList<Cinema> cinemasWithSameCityCode = new ArrayList<Cinema>();
 
     @Before
-    public void before() {
+    public void prepareData() {
         /*
-        * Insert 10 cinema into database
+        * Insert 10 random cinema into database
         * */
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -55,6 +59,22 @@ public class CinemaTester {
             Cinema afterc = cinemaRepository.save(cinema);
             cinemas.add(afterc);
         }
+
+        /*
+        * Insert 10 cinema with same citycode
+        * */
+        for (int i = 10; i < 10; i++) {
+            String name = "cinema" + Integer.toString(i);
+            String address = "address" + Integer.toString(i);
+            String phone = Integer.toString(i);
+            double lo = 0.5+0.1*i;
+            double la = 0.5+0.1*i;
+            int cityCode = 11;
+            Cinema cinema = new Cinema(name, address,phone, lo, la, cityCode);
+            Cinema afterc = cinemaRepository.save(cinema);
+            cinemasWithSameCityCode.add(afterc);
+        }
+        cinemaService.reload();
     }
 
     public Object[] cinemaToArray(Cinema a) {
@@ -72,11 +92,19 @@ public class CinemaTester {
     @Test
     public void queryTest() throws Exception {
 //        cinemaRepository.reload();
-        cinemaService.reload();
-        List<Cinema> cinemas = cinemaRepository.findAll();
         for (int i = 0; i < cinemas.size(); i++) {
             Cinema actual = cinemas.get(i);
             Cinema expe = cinemaService.findById(actual.getId());
+            Assert.assertArrayEquals(cinemaToArray(expe), cinemaToArray(actual));
+        }
+    }
+
+    @Test
+    public void locationTest() throws Exception {
+        List<Cinema> expel = cinemaDao.findByLocation(11, 0.5,0.5,0);
+        for (int i = 0; i < cinemasWithSameCityCode.size(); i++) {
+            Cinema actual = cinemasWithSameCityCode.get(i);
+            Cinema expe = expel.get(i);
             Assert.assertArrayEquals(cinemaToArray(expe), cinemaToArray(actual));
         }
     }
