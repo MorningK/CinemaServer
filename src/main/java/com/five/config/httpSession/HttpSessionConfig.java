01ca465644359ff2000af0245ac13c510f99688d
360782885@qq.com <360782885@qq.com>
2017-06-24 19:58:37 +0800

    redis config modify

--- src/main/java/com/five/config/httpSession/HttpSessionConfig.java
+++ src/main/java/com/five/config/httpSession/HttpSessionConfig.java
@@ -1,6 +1,7 @@
 package com.five.config.httpSession;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -8,15 +9,20 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 /**
  * Created by haoye on 17-6-7.
  */
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
+//public class HttpSessionConfig {
+//    @Bean
+//    public JedisConnectionFactory connectionFactory() {
+//        return new JedisConnectionFactory();
+//    }
+//
+//    @Bean
+//    public static ConfigureRedisAction configureRedisAction() {
+//        return ConfigureRedisAction.NO_OP;
+//    }
+//}
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
 public class HttpSessionConfig {
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public static ConfigureRedisAction configureRedisAction() {
-        return ConfigureRedisAction.NO_OP;
-    }
 }
+
